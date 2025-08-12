f5836b8d465245040528f6a016da23cb1bdfa913
felixcolahelllolz-design <felixcolahelllolz@gmail.com>
2025-08-12 04:06:37 -0400

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-import '../styles/globals.css';
-import dynamic from 'next/dynamic';
-const ResumePortfolio = dynamic(() => import('../components/ResumePortfolio'), { ssr: false });
-export default function Home() { return <ResumePortfolio />; }
