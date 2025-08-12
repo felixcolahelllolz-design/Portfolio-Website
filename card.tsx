87402028f653e293992dea2296cd6fe8fec3264b
felixcolahelllolz-design <felixcolahelllolz@gmail.com>
2025-08-12 03:41:20 -0400

    Delete card.tsx

--- card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { clsx } from "clsx";
-
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={clsx("rounded-2xl border shadow-sm", className)} {...props} />;
-}
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={clsx("p-4", className)} {...props} />;
-}
